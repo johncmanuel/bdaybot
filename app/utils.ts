6002b910a46c52378fdfa1af50d9f50e38a36f74
John Carlo Manuel <39042556+johncmanuel@users.noreply.github.com>
2024-11-23 13:09:14 -0800

    fix date input bug

--- app/utils.ts
+++ app/utils.ts
@@ -1,11 +1,16 @@
 // Assumes date is in MM/DD/YYYY or MM-DD-YYYY format
 export const validateDate = (date: string): boolean => {
-  const r = /^(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])-\d{4}$/;
+  const delim = date.includes("/") ? "/" : "-";
+  const escapedDelim = delim === "/" ? "\\/" : delim;
+
+  const r = new RegExp(
+    `^(0[1-9]|1[0-2])${escapedDelim}(0[1-9]|[12][0-9]|3[01])${escapedDelim}\\d{4}$`,
+  );
+
   if (!r.test(date)) {
     return false;
   }
 
-  const delim = date.includes("/") ? "/" : "-";
   const [month, day, year] = date.split(delim).map(Number);
   const d = new Date(year, month - 1, day);
 
